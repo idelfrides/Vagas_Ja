001467ba25da882dd624a29fbdd585eea88fb82e
Jairo Chaves <jairochaves2@gmail.com>
2018-06-10 14:44:25 -0300

    Correção do erro de abertura do home

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = '';
-$config['base_url'] = 'http://localhost/Vaga-ja/';
+$config['base_url'] = 'http://localhost/Vagas_Ja/';
 
 /*
 |--------------------------------------------------------------------------
